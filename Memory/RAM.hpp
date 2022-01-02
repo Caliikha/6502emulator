5bae3b61c7f6fdd8a4b0c677cf05663b94932e72
Caliikha <omaroov12@gmail.com>
2022-01-02 23:03:01 +0400

    Added [] operator to access memory with object name

--- Memory/RAM.hpp
+++ Memory/RAM.hpp
@@ -10,21 +10,25 @@ struct RAM {
     bit8_t Memory[Capacity_Limit];
 
     RAM() {
-        for (uint32_t i = 0; i < Capacity_Limit; i++){
+        for (uint32_t i = 0; i < Capacity_Limit; i++) {
             Memory[i] = 0x00;
         }
     }
 
     RAM(const bit8_t& def_set_value) {
-        for (uint32_t i = 0; i < Capacity_Limit; i++){
+        for (uint32_t i = 0; i < Capacity_Limit; i++) {
             Memory[i] = def_set_value;
         }
     }
 
     template <typename type = bit8_t>
     RAM(const std::initializer_list<type>& pre_set) {
-        for (int i = 0; i < pre_set.size(); i++){
+        for (int i = 0; i < pre_set.size(); i++) {
             Memory[i] = *(pre_set.begin() + i);
         }
+    }
+
+    bit8_t& operator[](const uint32_t& index) {
+        return this->Memory[index];
     }
 };
