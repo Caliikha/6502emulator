a96e194b414f58e0c54cabffb2ac300082fae647
Caliikha <omaroov12@gmail.com>
2022-01-02 20:49:17 +0400

    Changed the way the RAM module stores instructions from an initalizer list

--- Memory/RAM.hpp
+++ Memory/RAM.hpp
@@ -23,6 +23,9 @@ struct RAM {
 
     template <typename type = bit8_t>
     RAM(const std::initializer_list<type>& pre_set) {
-        std::copy(pre_set.begin(), pre_set.end(), Memory);
+        for (int i = 0; i < pre_set.size(); i++){
+            std::cout << i << '\n';
+            Memory[i] = *(pre_set.begin() + i);
+        }
     }
 };
